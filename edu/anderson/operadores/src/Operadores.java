41e0a9866a875bf20dee01a8b8e1468b3c15ff1f
Anderson César <anderson.cbcl3@gmail.com>
2023-05-03 20:11:04 +0000

    continuação com base na aula de operadores

---
+++ edu/anderson/operadores/src/Operadores.java
@@ -0,0 +1,31 @@
+public class Operadores {
+
+    // public static void main(String[] args) throws Exception {
+    //     String nomeCompleto = "LINGUAGEM" + "JAVA";
+    //     System.out.println(nomeCompleto);
+    // }
+
+    public static void main(String[] args) throws Exception {
+        String concatenacao ="?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);;
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+    }
+}
