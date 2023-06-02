aefc3af663d37111d06085697f81a59447e4ea91
Anderson César <anderson.cbcl3@gmail.com>
2023-06-02 19:21:06 +0000

    Continuando a aula com a parte de fluxos. Concluir

---
+++ edu/anderson/java-basico/operadores2/src/Fluxo.java
@@ -0,0 +1,16 @@
+public class Fluxo {
+    public static void main(String[] args) throws Exception {
+
+        ifFlecha();
+        ifSemFlecha();
+        ifFerias();
+        ifMenor();
+
+        switchSemana();
+        switchNumero();
+        switchFerias();
+
+    }
+
+    
+}
